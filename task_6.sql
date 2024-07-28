1fdac63af9d40a9b89b72f1a98d22e8d79bfebce
Ndumiso <ndumisovee@gmail.com>
2024-07-28 03:34:01 +0200

    Updated task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -10,9 +10,9 @@ mycursor = mydb.cursor()
 
 sql = "INSERT INTO customer (customer_id, customer_name, email, address) VALUES (%d, %s, %s, %s)"
 val = [
-("2", "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
-("3", "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
-("4", "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
+("2", "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
+("3", "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+("4", "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.")
 ]
 
 mycursor.executemany(sql, val)
