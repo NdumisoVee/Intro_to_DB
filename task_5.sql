da78fbf1936a0a82eb420db0a73b3c0c364acd5a
Ndumiso <ndumisovee@gmail.com>
2024-07-28 03:30:14 +0200

    Updated file

--- task_5.sql
+++ task_5.sql
@@ -9,6 +9,6 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 sql = "INSERT INTO customer (customer_id, customer_name, email, address) VALUES (%d, %s, %s, %s)"
-val = ("1", "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave".)
+val = ("1", "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
 mycursor.execute(sql, val)
 mydb.commit()
